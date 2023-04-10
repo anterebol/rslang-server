7c6b5eba33ba8eb3a27c204bb970d67ecd71877d
unknown <anterebol@gmail.com>
2023-04-10 19:15:59 +0300

    fix: fix cors policy

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
 app.use(helmet());
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 app.use('/files', express.static(path.join(__dirname, '../files')));
